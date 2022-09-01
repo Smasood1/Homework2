672f3ea97f4fe6bd1ea79be16b7bac992dab7e47
Smasood1 <saadmasood219@gmail.com>
2022-08-31 21:21:09 -0400

    homework2 java

--- src/main/java/Homework2.java
+++ src/main/java/Homework2.java
@@ -1,6 +1,54 @@
 public class Homework2 {
     public static void main(String[] args) {
-        System.out.println("yo");
+        System.out.println("yo im doing my second homework");
+
+        System.out.println("i guess this is my second line");
+
+        //This is a single line comment.
+
+        // Java Variables
+
+        String name = "Saad";
+        int age = 24;
+        float gpa = 4.0f;
+        char sex = 'M';
+        Boolean hotornot = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(gpa);
+        System.out.println(sex);
+
+        //using String to print messages + variables
+
+        System.out.println(" My name is" + " " + name);
+        System.out.println(" I am " + age + " " + "years old");
+        System.out.println(" I have a " + " " + gpa + " " + "gpa");
+        System.out.println("My Sex is" + " " + sex);
+        System.out.println("Saad is the Hottest guy on earth" + " " + hotornot);
+
+
+        //else if statements
+
+        int apples = 30;
+        if (apples <= 30) {
+            System.out.println("we need some more apples");
+        } else if (apples < 20) {
+            System.out.println("we are gonna die of starvation");
+        } else {
+            System.out.println("thats enough apples");
+        }
+
+
+
+
+
+
+
+
+
 
     }
 }
+
+
